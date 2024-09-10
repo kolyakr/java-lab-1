057d0429c41e62dfda783bb92c6fefdc42f4fd2f
Mykola Kryvoruchko <krivoruchko.kolya@gmail.com>
2024-09-10 23:32:14 +0300

    add method getLastDigit()

--- src/LucasNumber.java
+++ src/LucasNumber.java
@@ -1,14 +1,20 @@
 public class LucasNumber {
-    public int number;
-    public int value;
+    private int number;
+    private long value;
 
-    public LucasNumber(int number, int value){
+    public LucasNumber(int number, long value){
         System.out.println("create constructor");
         this.number = number;
         this.value = value;
     }
 
-    public void getValue(){
+    public void getValue() {
         System.out.println("Number: " + this.number + "\nValue: " + this.value);
+    }
+
+    public int getLastDigit(){
+        long copyValue = this.value;
+        int lastNumber = (int)(copyValue % 10);
+        return lastNumber;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        LucasNumber a = new LucasNumber(1, 2);
+        LucasNumber a = new LucasNumber(10, 24897);
         a.getValue();
+        int lastDigit = a.getLastDigit();
+        System.out.println("last digit: " + lastDigit);
     }
 }
