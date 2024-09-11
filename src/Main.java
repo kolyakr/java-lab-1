1199730f03e773d812b19adfb716f64872c4233a
Mykola Kryvoruchko <krivoruchko.kolya@gmail.com>
2024-09-11 20:51:58 +0300

    fix generateLucasNumbers() and add printSameLastDigitsNums()

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Write number of Lucas numbers row: ");
-        int N = scanner.nextInt();
-
-        generateLucasNumbers(N);
+        int N = 0;
+        int digit = 0;
+        try{
+            System.out.println("Write number of Lucas numbers row: ");
+            N = scanner.nextInt();
+            System.out.println("Write digit: ");
+            digit = scanner.nextInt();
+            LucasNumber[] array = generateLucasNumbers(N);
+            printLucasNumbers(array);
+            printSameLastDigitNums(array, digit);
+        }catch(RuntimeException e){
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
-    public static void generateLucasNumbers(int N){
+    public static LucasNumber[] generateLucasNumbers(int N){
         if(N <= 0){
             System.out.println("Invalid input. Please write number bigger than 0");
-            return;
+            return null;
         }
         LucasNumber[] array = new LucasNumber[N];
 
@@ -31,14 +41,36 @@ public class Main {
            array[i] = x;
        }
 
-        printLucasNumbers(array);
+        return array;
     }
 
     public static void printLucasNumbers(LucasNumber[] array){
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+        System.out.println("Lucas numbers row:");
         for(int i = 0; i < array.length; i++){
             if(array[i] != null){
                 array[i].print();
             }
         }
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+    }
+
+    public static void printSameLastDigitNums(LucasNumber[] array, int digit){
+        int number = 0;
+        System.out.println("Numbers with the same last digit(" + digit + "):");
+        for(int i = 0; i < array.length; i++){
+            int numsDigit = array[i].getLastDigit();
+            if(numsDigit == digit){
+                array[i].print();
+                number++;
+            }
+        }
+
+        if(number != 0){
+            System.out.println("General number: " + number);
+        }else{
+            System.out.println("Row of Lucas numbers don`t have numbers with the same last digits");
+        }
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
     }
 }
