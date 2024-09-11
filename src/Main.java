68d462fc777777f18ed9cb8fa2ce511b624250af
Mykola Kryvoruchko <krivoruchko.kolya@gmail.com>
2024-09-11 19:00:41 +0300

    add demo method generateLucasNumber()

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
 
 public class Main {
     public static void main(String[] args) {
-        LucasNumber a = new LucasNumber(10, 24897);
-        a.getValue();
-        int lastDigit = a.getLastDigit();
-        System.out.println("last digit: " + lastDigit);
+        generateLucasNumbers(10);
+    }
+
+    public static void generateLucasNumbers(int N){
+       if(N <= 0){
+           System.out.println("Invalid input. Please write number bigger than 0");
+           return;
+       }
+
+       long firstNum = 2;
+       long secondNum = 1;
+       for(int i = 1; i <= N; i++){
+           if(i == 1 || i == 2){
+               int value = i == 1 ? 2 : 1;
+               System.out.println("Number: " + i + ", value: " + value);
+           }else{
+               long value = firstNum + secondNum;
+               firstNum = secondNum;
+               secondNum = value;
+               System.out.println("Number: " + i + ", value: " + value);
+           }
+       }
     }
 }
